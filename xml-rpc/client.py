93fca5d808e2a9464ac2c1bfc893e76f0057d2e7
Angel Parra <angel.parra@estudiants.urv.cat>
2022-06-16 22:36:28 +0200

    Changes by User

--- xml-rpc/client.py
+++ xml-rpc/client.py
@@ -12,25 +12,11 @@ worker_server = xmlrpc.client.ServerProxy('http://localhost:9000')
 while worker_server.assign_worker() != "Worker assigned":
     sleep(2)
 
-result_read = timeit.timeit(stmt='worker_server.read_csv(mydf)', globals=globals(), number=1)
-result_max = timeit.timeit(stmt='worker_server.minimum(mydf)', globals=globals(), number=1)
-result_min = timeit.timeit(stmt='worker_server.maximum(mydf)', globals=globals(), number=1)
-result_isin = timeit.timeit(stmt='worker_server.isin(mydf, [0,50])', globals=globals(), number=1)
-result_col = timeit.timeit(stmt='worker_server.columns(mydf)', globals=globals(), number=1)
-result_head = timeit.timeit(stmt='worker_server.head(mydf)', globals=globals(), number=1)
-result_by = timeit.timeit(stmt='worker_server.groupby(mydf, "LatD")', globals=globals(), number=1)
-#print(result_read)
-#print(result_max)
-#print(result_min)
-#print(result_read)
-#print(result_isin)
-#print(result_col)
-#print(result_head)
-#print(result_by)
 
 
 ### TEST FUNCTIONS OK  // TODAS OK
-#print("\nMin:"+worker_server.minimum(mydf))
+print("\nMin:"+worker_server.read_csv(mydf))
+print("\nMin:"+worker_server.minimum(mydf))
 #print("\nMax:"+worker_server.maximum(mydf))
 #print("\nIsin:"+worker_server.isin(mydf, [0,50]))
 #print("\nColumns:"+worker_server.columns(mydf))
--- xml-rpc/master.py
+++ xml-rpc/master.py
@@ -12,9 +12,11 @@ server = SimpleXMLRPCServer(
 
 worker_list = list()
 
-def register_worker(port):
-    worker_list.append(port)
 
+def register_worker(port):
+    worker_list.append(xmlrpc.client.ServerProxy('http://localhost:'+port))
+    for worker in worker_list:
+        worker.worker_list = worker_list
 
 def list_workers():
     return worker_list if worker_list else None
@@ -23,8 +25,75 @@ def list_workers():
 def assign_worker():
     worker_port = worker_list[0] if worker_list else None
     # worker_list.pop(0)
-    return worker_port
+    return "Worker assigned"
 
+
+def read_csv(filepath):
+
+    for worker in worker_list:
+        df = worker.openDF[filepath] = dd.read_csv(filepath)
+
+
+    return "CSV read"
+
+
+# numpy.sum can be replaced.
+def apply(filepath):
+    return str(openDF.get(filepath).apply(numpy.sum))
+
+
+def columns(filepath):
+    return str(openDF.get(filepath).columns)
+
+
+# Averages with matching numbers, if there are 2 matching values, it will average the entire row. EX: LATD. MEAN
+# can replaced.
+def groupby(filepath, by):
+    return str(openDF.get(filepath).groupby(openDF.get(filepath)[by]).mean())
+
+
+# Return a N elements (DEFAULT N=5)
+def head(filepath, num=5):
+    return str(openDF.get(filepath).head(num))
+
+    # Test if cells contain values
+
+
+def isin(filepath, values):
+    return str(openDF.get(filepath).isin(values))
+
+    # Iterate function.
+
+
+def items(filepath):
+    df_str = ''
+    for label, value in openDF[filepath].items():
+        df_str += f'label: {label}\n'
+        df_str += f'content:\n {value}\n'
+
+    return str(df_str)
+    # Return the maximum of the values
+
+
+def maximum(filepath):
+    return str(openDF.get(filepath).max(numeric_only='True'))
+
+
+# Return the minimum of the values
+
+def minimum(filepath):
+    return str(openDF.get(filepath).min(numeric_only='True'))
+
+
+server_worker.register_function(read_csv, "read_csv")
+server_worker.register_function(apply, "apply")
+server_worker.register_function(columns, "columns")
+server_worker.register_function(groupby, "groupby")
+server_worker.register_function(head, "head")
+server_worker.register_function(isin, "isin")
+server_worker.register_function(items, "items")
+server_worker.register_function(maximum, "maximum")
+server_worker.register_function(minimum, "minimum")
 
 server.register_function(register_worker, "register_worker")
 server.register_function(list_workers, "list_workers")
